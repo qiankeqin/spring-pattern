6c5d5c3f762c68a83b8c3ab4e900d604c4e28a63
qiankeqin <qiankeqin@51xf.cn>
2019-07-05 09:18:23 +0800

    observer

---
+++ src/main/java/com/dayu/pattern/observer/ObserverTest.java
@@ -0,0 +1,12 @@
+package com.dayu.pattern.observer;
+
+/**
+ * @author qiankeqin
+ * @Description: DESCRIPTION
+ * @date 2019-07-05 09:18
+ */
+public class ObserverTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/dayu/pattern/observer/subject/Subject.java
@@ -0,0 +1,16 @@
+package com.dayu.pattern.observer.subject;
+
+/**
+ * @author qiankeqin
+ * @Description: DESCRIPTION
+ * @date 2019-07-04 20:12
+ */
+public class Subject {
+    public void add(){
+        System.out.println("add");
+    }
+
+    public void remove(){
+        System.out.println("remove");
+    }
+}
